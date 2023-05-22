c84d88ffd3a1e6a3ce8eb521f3101cae71e56543
sejong-assistant <133758341+soultree1@users.noreply.github.com>
2023-05-22 20:41:20 +0900

    PQW2ECI

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -1,12 +1,12 @@
 function A=PQW2ECI(arg_prg, inc_angle, RAAN)
 
-%% input
-X=input('X='); %3X3 matrix
-arg_prg=input('arg_prg='); %argument of perigee
-inc_angle=input('inc_angle='); % inclination
-RAAN=input('RAAN='); %right ascension of ascending node
 
-%% simplification
+%% input
+arg_prg=input('arg_prg ='); %argument of perigee
+inc_angle=input('inc_angle='); %inclination
+RAAN=input('RAAN ='); %right ascension of ascending node
+
+%% simplication
 a=cos(arg_prg);
 b=sin(arg_prg);
 c=cos(inc_angle);
@@ -15,4 +15,4 @@ e=cos(RAAN);
 f=sin(RAAN);
 
 %% calculation
-A=[e f 0; -f e 0; 0 0 1]*[1 0 0; 0 c d; 0 -d c]*[a b 0; -b a 0; 0 0 1]*X;
+A=[e f 0; -f e 0; 0 0 1]*[1 0 0; 0 c d; 0 -d c]*[a b 0; -b a 0; 0 0 1];
