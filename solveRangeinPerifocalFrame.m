b6859016654ac3853466047fcc340f29d5c8e58e
sejong-assistant <133758341+soultree1@users.noreply.github.com>
2023-05-29 22:00:46 +0900

    range in perifocal

--- solveRangeinPerifocalFrame.m
+++ solveRangeinPerifocalFrame.m
@@ -4,8 +4,8 @@ mu= 398600.4418; % km^3Xs^-2
 semimajor_axis = input('semimajor_axis = ');
 eccentricity = input('eccentricity = ');
 true_anomaly = input('true anomaly = ');
-cosE=(eccentricity+cos(true_anomaly))/(1+eccentricity*cos(true_anomaly)); %cos(E)
+cosE=(eccentricity+cos(true_anomaly*pi/180))/(1+eccentricity*cos(true_anomaly*pi/180)); %cos(E)
 r=semimajor_axis*(1-eccentricity*cosE);
 
-rangeinPQW = [r*cos(true_anomaly); r*sin(true_anomaly); 0];
+rangeinPQW = [r*cos(true_anomaly*pi/180); r*sin(true_anomaly*pi/180); 0];
 end
