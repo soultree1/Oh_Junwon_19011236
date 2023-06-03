810dcacb0fa699857356831c66374ec41e06d569
sejong-assistant <133758341+soultree1@users.noreply.github.com>
2023-06-03 16:57:23 +0900

    .

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -7,12 +7,12 @@ inc_angle=input('inc_angle='); %inclination
 RAAN=input('RAAN ='); %right ascension of ascending node
 
 %% simplification
-a=cos(arg_prg);
-b=sin(arg_prg);
-c=cos(inc_angle);
-d=sin(inc_angle);
-e=cos(RAAN);
-f=sin(RAAN);
+a=cos(arg_prg*pi/180);
+b=sin(arg_prg*pi/180);
+c=cos(inc_angle*pi/180);
+d=sin(inc_angle*pi/180);
+e=cos(RAAN*pi/180);
+f=sin(RAAN*pi/180);
 
 %% calculation
 A=[e f 0; -f e 0; 0 0 1]*[1 0 0; 0 c d; 0 -d c]*[a b 0; -b a 0; 0 0 1];
